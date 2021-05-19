4122322a76764e0b9fc1655844ba216d3fc0a85a
Joseph R. Stewart <JosephRobertStewart@gmail.com>
2021-05-19 11:32:47 -0600

    Update word.py
    Looks great! Way to add the word bank!

--- jumper/game/word.py
+++ jumper/game/word.py
@@ -5,9 +5,10 @@ class Word:
     """A class to choose the word, decide if letters are found, and keep track of found letters
     
     Stereotype:
-        Information Holder ##Not sure about this one yet.
-
+        Information Holder
+        
     Attributes:
+        (panda data/text file)
         
     """
 
@@ -28,12 +29,12 @@ class Word:
             word (str) -- Word for the game
 
         """
-        df = pd.read_csv("wordbank.csv")
-        column = df["category"]
-        unique = column.unique()
-        options = unique.tolist()
+        df = pd.read_csv("wordbank.csv") # the dataframe 
+        column = df["category"] 
+        unique = column.unique() # 4 catagories
+        options = unique.tolist() 
 
-        valid = False
+        valid = False ## This is the user picking a category.
         while not valid:
             print("Please choose a category:")
             for option in options:
@@ -46,11 +47,11 @@ class Word:
                 print(f"{category.title()} is not a category, please choose one from this list:\n {options}")
 
 
-        filtered_df = df[(df["category"] == category)]
-        word_choices = filtered_df["word"]
-        print(word_choices)
-        print(word_choices.unique().shape[0])
-        num = random.randint(1,word_choices.unique().shape[0])
-        word = word_choices.iloc[num-1]
-        print(word)
+        filtered_df = df[(df["category"] == category)] ## Filters the words by category (i.e. 
+        word_choices = filtered_df["word"] ## sorts the words
+#         print(word_choices) ## Used for testing
+#         print(word_choices.unique().shape[0])
+        num = random.randint(1,word_choices.unique().shape[0]) ## Adjusts the random based on category size.
+        word = word_choices.iloc[num-1] ## Picks a word based on the random word.
+#         print(word) ## Used for testing
         return word
