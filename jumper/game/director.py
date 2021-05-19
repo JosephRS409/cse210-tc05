2c3f770d8352ca74985d72c28136abfe76d0dcdf
Austin Jesperson <jes16003@byui.edu>
2021-05-19 10:49:33 -0600

    Added wordbank and a method to chose a word for the game

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,6 +1,8 @@
 from game.console import Console
 from game.player import Player
 from game.jumper import Jumper
+import pandas as pd
+import random
 
 class Director:
     """A code template for a person who directs the game. The responsibility of 
@@ -26,3 +28,40 @@ class Director:
         self.player = Player()
         self.keep_playing = True
         self.jumper = Jumper()
+
+    def get_word(self):
+        """Chooses a word for the game
+
+        Args:
+            self - Instance of the director
+
+        Returns:
+            word (str) -- Word for the game
+
+        """
+        df = pd.read_csv("wordbank.csv")
+        column = df["category"]
+        unique = column.unique()
+        options = unique.tolist()
+
+        valid = False
+        while not valid:
+            print("Please choose a category:")
+            for option in options:
+                print(option.title())
+            category = input("\n>")
+
+            if category.lower() in options:
+                valid = True
+            else:
+                print(f"{category.title()} is not a category, please choose one from this list:\n {options}")
+
+
+        filtered_df = df[(df["category"] == category)]
+        word_choices = filtered_df["word"]
+        print(word_choices)
+        print(word_choices.unique().shape[0])
+        num = random.randint(1,word_choices.unique().shape[0])
+        word = word_choices.iloc[num-1]
+        print(word)
+        return word
