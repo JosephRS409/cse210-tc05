e4e43ba0713d885b8fa1a18e690398a21d695fa3
Joseph R. Stewart <JosephRobertStewart@gmail.com>
2021-05-20 20:48:31 -0600

    committed

--- jumper/game/director.py
+++ jumper/game/director.py
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-from game.console import Console
-from game.word import Word
-=======
 from game.player import Player
->>>>>>> master
 from game.jumper import Jumper
 from game.word import Word
 
@@ -27,12 +22,7 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
-<<<<<<< HEAD
-        self.console = Console()
-        self.word = Word()
-=======
         self.player = Player()
->>>>>>> master
         self.keep_playing = True
         self.jumper = Jumper()
         self.word = Word()
@@ -65,19 +55,6 @@ class Director:
             self.placeholder.append("_") # Makes the "_" per letter.
 
     def update_guesses(self):
-<<<<<<< HEAD
-        """Updates the guesses and parachute"""
-        # Side note I don't think we need a console and a player class,
-        #  and i'm not sure which we'll keep so for now i'll pretend we're gonna use console to get user input
-        if self.console.guess in self.letters:
-            iterations = self.letters.count(self.console.guess)
-            for _ in range(0, iterations):
-                list_index  = self.letters.index(self.console.guess)
-                self.placeholder.pop(list_index)
-                self.placeholder.insert(list_index, self.console.guess)
-                self.letters.pop(list_index)
-                self.letters.insert(list_index, "")
-=======
         """Updates the guesses and parachute
         
         Args:
@@ -116,7 +93,6 @@ class Director:
             self.jumper.parachute[self.i] = " " # Replaces the next part of the parachute 
                                                      # with a  blank
             self.i +=1 # This adds 1 to a counter for incorrect guesses
->>>>>>> master
 
     def display_word(self):
         """Displays amount of letters in the word as _ for the player to guess
@@ -130,73 +106,6 @@ class Director:
         for thing in self.placeholder: # Displays the word with a space between each letter
             word = word + " " + thing
         print(word)
-<<<<<<< HEAD
-        while self.keep_playing:
-            self.get_inputs() # First display the prompt and get the inputs.
-                                # via Console class
-            self.do_updates() #
-            self.do_outputs()
-
-    def get_inputs(self):
-        """Gets the inputs at the beginning of each round of play. In this case,
-        that means getting a letter from the User in the Jumper class via the 
-        console class.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        message = self.hunter.get_message()
-        self.console.write(message)
-        location = self.console.read_number("Enter a location [1-1000]: ") # User input here.
-        self.hunter.move(location) # This is the new location of the hunter.
-        
-    def do_updates(self):
-        """Updates the important game information for each round of play. In 
-        this case, the .
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        self.rabbit.watch(self.hunter.location) # The location of the hunter is sent to the 
-            # rabbit watch method.
-        
-    def do_outputs(self):
-        """Outputs the important game information for each round of play. In 
-        this case, that means the rabbit provides a hint.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        hint = self.rabbit.get_hint()
-        self.console.write(hint)
-        self.keep_playing = (self.rabbit.distance[-1] != 0)
-        """
-        def set_gamemode(self):
-        choice = input("Which gamemode would you like to play on? Easy or Normal? E/N: ")
-
-        if choice.lower() == "n":
-            self.mode = "normal"
-        elif choice.lower() == "e":
-            self.mode = "easy"
-
-    def set_jumper(self):
-        """Sets the parachute based on the game difficulty
-        
-        Args: 
-            self (Screen): An instance of Screen.
-        """
-        if self.mode == "normal":
-            self.parachute = ["___","/___\\","\\   /","\\ /","0"]
-        elif self.mode == "easy":
-            self.parachute = ["___","/___\\","\\","/","\\","/","0"]
-
-    def display_jumper(self):
-        if len(self.parachute) == 7:
-            print(f" {self.parachute[0]}\n{self.parachute[1]}\n{self.parachute[2]}   {self.parachute[3]}\n {self.parachute[4]} {self.parachute[5]}\n  {self.parachute[6]}\n /|\\\n / \\")
-        elif len(self.parachute) == 5:
-            print(f" {self.parachute[0]}\n{self.parachute[1]}\n{self.parachute[2]}\n {self.parachute[3]}\n  {self.parachute[4]}\n /|\\\n / \\")
-
-=======
 
     def able_to_play(self):
         """Check to see if the player wins/can continue to play
@@ -235,4 +144,3 @@ class Director:
             self.player.guess_letter() # Has player guess a letter.
             self.update_guesses() # Updates the lists with the guesses.
             self.keep_playing = self.able_to_play() #Checks to see if you can keep playing.
->>>>>>> master
